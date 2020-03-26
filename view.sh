8806ca2c4d8c9d4c52d85fba08cf5c4be30a5214
Chad Spensky <chad@allthenticate.net>
2020-03-26 11:47:16 -0700

    adding fixing build and adding trust.io

--- view.sh
+++ view.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #hugo --i18n-warnings server
-HUGO_THEME=academic hugo --themesDir themes -p 1315 --i18n-warnings server
+HUGO_THEME=academic hugo --themesDir themes -p 1315 --buildFuture --i18n-warnings server
 
